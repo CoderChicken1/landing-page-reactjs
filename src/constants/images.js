d5354aa7620f40312b2c7bb6ac4b59e906248434
CoderChicken1 <genjitar0.12@gmail.com>
2023-10-27 15:33:05 +0700

    submit

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,24 +1,8 @@
 import header_bg from "../assets/images/header-bg.png";
 import coming_bg from "../assets/images/coming-bg.jpg";
-import logo from "../assets/images/logo.png";
-import signature from "../assets/images/signature.png";
-import icon_archer from "../assets/images/icon-archer.png"; 
-import icon_briefcase from "../assets/images/icon-briefcase.png";
-import icon_clock from "../assets/images/icon-clock.png";
-import icon_console from "../assets/images/icon-console.png";
-import icon_diamond from "../assets/images/icon-diamond.png";
-import icon_eye from "../assets/images/icon-eye.png";
-import icon_fantasy from "../assets/images/icon-fantasy.png";
-import icon_heart from "../assets/images/icon-heart.png";
-import icon_paint from "../assets/images/icon-paint.png";
-import icon_phone from "../assets/images/icon-phone.png";
-import icon_plane from "../assets/images/icon-plane.png";
-import icon_star from "../assets/images/icon-star.png";
-import icon_star_white from "../assets/images/icon-star-white.png";
 import icon_link from "../assets/images/icon_link.png";
 import icon_insta from "../assets/images/icon-insta.png";
 
-import skills_bg from "../assets/images/skills-bg.jpg";
 import about_us from "../assets/images/about-us.png";
 import introduce from "../assets/images/introduce.png";
 import footer_bg from "../assets/images/footer_bg.png";
@@ -45,24 +29,8 @@ export default{
     about_us,
     header_bg,
     coming_bg,
-    logo,
-    signature,
-    icon_archer,
-    icon_briefcase,
-    icon_clock, 
-    icon_console,
-    icon_diamond,
-    icon_eye,
-    icon_fantasy, 
-    icon_heart,
-    icon_paint,
-    icon_phone,
-    icon_plane,
-    icon_star,
-    icon_star_white,
     icon_link,
     icon_insta,
-    skills_bg,
     footer_bg,
     p_1,
     p_2,
